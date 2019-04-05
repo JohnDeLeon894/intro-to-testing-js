360b1fa2b0647bf0b7693176c9a29946b4b03aff
John De Leon <johndeleon@MacBook-Pro-3.local>
2019-04-05 14:19:15 -0500

    added more test and resolved

--- code.js
+++ code.js
@@ -10,7 +10,16 @@ const helloWorld = function() {
 };
 
 
-function sayHello() {
-    
+function sayHello(input) {
+
+    // if (input === "Jane") {
+    //     return "Hello, Jane!";
+    // }else if (input=== "Alex") {
+    //     return "Hello, Alex!";
+    // }else if(input==="Pat"){
+    //     return "Hello, Pat!";
+    // }
+    // return "";
+    return "Hello, " + input + "!";
 }
 
--- tests.js
+++ tests.js
@@ -21,6 +21,17 @@ describe('sayHello', function(){
     it('should be a defined function', function () {
         expect(typeof sayHello).toBe('function');
     });
-
+    it('should return a string when called', function () {
+        expect(typeof sayHello()).toBe('string');
+    });
+    it('should return "Hello, Jane!" when called with sayHello("Jane")', function () {
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
+    });
+    it('should return "Hello, Alex!" when executed with sayHello("Alex")', function f() {
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
+    });
+    it("should return 'Hello, Pat!' when executed)", function () {
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
+    });
 
 });
